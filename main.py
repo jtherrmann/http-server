98c141f4e40b1ef60c5e88f58c4ff86b03742977
Jake Herrmann <jtherrmann@alaska.edu>
2019-03-09 00:17:03 -0900

    Add TODOs to learn about and document details of socket interface.

--- main.py
+++ main.py
@@ -6,8 +6,15 @@ from typing import Iterator
 # - https://docs.python.org/3/library/socket.html#example
 
 
+# TODO: understand (& comment where appropriate) the purpose of each line
+
+
 def run_server(host: str, port: int) -> Iterator[str]:
-    with socket.socket() as listener:
+
+    # TODO: record info from man 2 socket on the params to socket; these are
+    # already the default values, but specify them explicitly for educational
+    # purposes
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as listener:
         listener.bind((host, port))
         listener.listen()
 
