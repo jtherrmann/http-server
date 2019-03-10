4184954b6480fe1e92335131cb5ee201ec83c758
Jake Herrmann <jtherrmann@alaska.edu>
2019-03-09 16:00:56 -0900

    Ensure that the server is killed after a test fails.

--- test.py
+++ test.py
@@ -4,12 +4,33 @@ import time
 import unittest
 
 
-class ServerTestCase(unittest.TestCase):
+class ServerEchoTestCase(unittest.TestCase):
 
-    def test_run_server_single_echo(self) -> None:
-        proc = subprocess.Popen(('python3', 'test_run_server_echo.py'))
+    # TODO: put these in __init__
+    # self._script = 'test_run_server_echo.py'
+    # self._server = None
+
+    def setUp(self) -> None:
+        self._script = 'test_run_server_echo.py'  # TODO: temp
+        self._server = subprocess.Popen(('python3', self._script))
         time.sleep(1)
 
+    def tearDown(self) -> None:
+        # SIGTERM vs. SIGKILL: https://major.io/2010/03/18/sigterm-vs-sigkill/
+
+        print('Sending SIGTERM to process {}.'.format(self._server.pid))
+        self._server.terminate()
+        time.sleep(1)
+
+        while self._server.poll() is None:
+            print(
+                'Process {} still alive. '
+                'Sending SIGKILL.'.format(self._server.pid)
+            )
+            self._server.kill()
+            time.sleep(1)
+
+    def test_run_server_single_echo(self) -> None:
         # TODO: don't re-specify socket params here?
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as client:
             client.connect(('127.0.0.1', 8080))
@@ -19,23 +40,14 @@ class ServerTestCase(unittest.TestCase):
 
         self.assertEqual(response, b'Hello, there!')
 
+        # TODO: doc this problem where we check that the server isn't running
+
         # TODO: problem: if above assertion fails (or any other exception is
         # raised, probably) before the terminate line below, the proc is never
         # terminated, though somehow we can still run the test program again
         # (is it b/c we allow reusing the same port?); should fix this issue
         # and also, at the start of the test, check that the server is not
         # currently running
-
-        # SIGTERM vs. SIGKILL: https://major.io/2010/03/18/sigterm-vs-sigkill/
-
-        print('Sending SIGTERM to process {}.'.format(proc.pid))
-        proc.terminate()
-        time.sleep(1)
-
-        while proc.poll() is None:
-            print('Process {} still alive. Sending SIGKILL.'.format(proc.pid))
-            proc.kill()
-            time.sleep(1)
 
 
 if __name__ == '__main__':
