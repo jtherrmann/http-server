5fbb090b32b35ee3be23d4b9c811d2bba2cbc3e7
Jake Herrmann <jtherrmann@alaska.edu>
2019-03-09 23:43:22 -0900

    Pass test_triple_caps_single.

--- server_triple_caps.py
+++ server_triple_caps.py
@@ -2,4 +2,4 @@ import server
 
 
 if __name__ == '__main__':
-    server.run_server('127.0.0.1', 8080, lambda request: request)
+    server.run_server('127.0.0.1', 8080, lambda request: request.upper() * 3)
