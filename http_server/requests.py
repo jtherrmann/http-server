1f093d2e67b465b271cb2a2bbba23c199133961d
Jake Herrmann <jtherrmann@alaska.edu>
2019-03-11 14:43:23 -0800

    Add TODOs for more tests.

--- http_server/requests.py
+++ http_server/requests.py
@@ -29,6 +29,13 @@ HTTP_VERSION = 'HTTP/1.1'
 # <version>       = HTTP_VERSION
 
 
+# TODO: tests:
+# - missing spaces, missing \r\n, or extra whitespace in request-line
+# - invalid method
+# - whitespace or / in uri-part-body
+# - invalid version
+
+
 @attrs(frozen=True)
 class Request:
     method = attrib()  # type: str
