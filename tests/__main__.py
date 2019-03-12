ac3e5524a84d6ac0c8d990a7f52198f9f117f72e
Jake Herrmann <jtherrmann@alaska.edu>
2019-03-12 11:32:59 -0800

    Add test_responses.py

--- tests/__main__.py
+++ tests/__main__.py
@@ -3,6 +3,7 @@ import unittest
 
 
 if __name__ == '__main__':
+    # TODO: also check cwd when running individual test files
     tests_path = os.path.dirname(os.path.abspath(__file__))
     project_path = os.path.dirname(tests_path)
     if os.getcwd() != project_path:
@@ -13,4 +14,5 @@ if __name__ == '__main__':
     else:
         from test_server import ServerEchoTestCase, ServerTripleCapsTestCase  # noqa F401
         from test_requests import RequestsTestCase  # noqa F401
+        from test_responses import ResponsesTestCase  # noqa F401
         unittest.main()
