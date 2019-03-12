2b91f08fe7594c055834ead4d736343a66b35e3e
Jake Herrmann <jtherrmann@alaska.edu>
2019-03-12 13:19:54 -0800

    Add test_handler (unfinished).

--- tests/test_handlers.py
+++ tests/test_handlers.py
@@ -1,10 +1,26 @@
 import unittest
 
+from http_server.requests import Request
+from http_server.responses import Response
+from http_server.tokens import GET_METHOD, HTTP_VERSION
+
 
 class HandlersTestCase(unittest.TestCase):
 
-    def test(self) -> None:
-        pass
+    def test_handler(self) -> None:
+        # TODO: finish
+        def custom_handler(request: Request) -> Response:
+            message_body = 'You requested URI {}'.format(request.uri)
+            return Response(200, ('text', 'plain'), message_body)
+
+        self.assertEqual(
+            custom_handler(
+                Request(GET_METHOD, ['hello', 'world'], HTTP_VERSION)
+            ),
+            Response(
+                200, ('text', 'plain'), "You requested URI ['hello', 'world']"
+            )
+        )
 
 
 if __name__ == '__main__':
