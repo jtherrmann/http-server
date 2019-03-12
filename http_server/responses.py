8e9b4e726551aa35b2e04bb134e5fec8e36e80b7
Jake Herrmann <jtherrmann@alaska.edu>
2019-03-12 12:06:30 -0800

    Pass tests for invalid responses.

--- http_server/responses.py
+++ http_server/responses.py
@@ -1,14 +1,27 @@
 from typing import Tuple  # noqa F401
 
-from attr import attrs, attrib
-
 
 # TODO: move HTTP_VERSION to this file, import it in requests (w/ TODO to
 # include more versions in grammar)
 
 
-@attrs(frozen=True)
 class Response:
-    status_code = attrib()  # type: int
-    content_type = attrib()  # type: Tuple[str, str]
-    message_body = attrib()  # type: str
+
+    _status_codes = (200,)
+    _content_types = (('text', 'html'),)
+
+    def __init__(
+            self,
+            status_code: int,
+            content_type: Tuple[str, str],
+            message_body: str) -> None:
+
+        if status_code not in self._status_codes:
+            raise ValueError()
+
+        if content_type not in self._content_types:
+            raise ValueError()
+
+        self._status_code = status_code
+        self._content_type = content_type
+        self._message_body = message_body
