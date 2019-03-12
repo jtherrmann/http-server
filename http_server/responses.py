50035b0b018dbb966477f2c2bf5d5378635c1c30
Jake Herrmann <jtherrmann@alaska.edu>
2019-03-12 12:42:29 -0800

    Pass test_response_get_str.

--- http_server/responses.py
+++ http_server/responses.py
@@ -1,12 +1,16 @@
 from typing import Tuple  # noqa F401
 
+from .requests import HTTP_VERSION, CRLF
+
 
 # TODO: move HTTP_VERSION and CRLF to shared module
 
 
 class Response:
 
-    _status_codes = (200,)
+    # https://tools.ietf.org/html/rfc2616#section-6.1.1
+    _code_phrases = {200: 'OK'}
+
     _content_types = (('text', 'html'), ('text', 'plain'))
 
     def __init__(
@@ -15,7 +19,7 @@ class Response:
             content_type: Tuple[str, str],
             message_body: str) -> None:
 
-        if status_code not in self._status_codes:
+        if status_code not in self._code_phrases:
             raise ValueError()
 
         if content_type not in self._content_types:
@@ -26,4 +30,28 @@ class Response:
         self._message_body = message_body
 
     def get_str(self) -> str:
-        pass
+        # https://tools.ietf.org/html/rfc2616#section-6
+        return (
+            self._get_status_line()
+            + self._get_content_type() + CRLF
+            + self._get_content_length() + CRLF
+            + CRLF + self._message_body
+        )
+
+    def _get_status_line(self) -> str:
+        # https://tools.ietf.org/html/rfc2616#section-6.1
+        return '{} {} {}{}'.format(
+            HTTP_VERSION, self._status_code, self._get_reason_phrase(), CRLF
+        )
+
+    def _get_reason_phrase(self) -> str:
+        return self._code_phrases[self._status_code]
+
+    def _get_content_type(self) -> str:
+        # https://tools.ietf.org/html/rfc2616#section-14.17
+        # https://tools.ietf.org/html/rfc2616#section-3.7
+        return 'Content-Type: ' + '/'.join(self._content_type)
+
+    def _get_content_length(self) -> str:
+        # https://tools.ietf.org/html/rfc2616#section-14.13
+        return 'Content-Length: {}'.format(len(self._message_body.encode()))
