43c255c3651be7f7957db4a1d893ffcd8ccee203
Jake Herrmann <jtherrmann@alaska.edu>
2019-03-12 15:53:53 -0800

    Pass recently added response tests.

--- http_server/responses.py
+++ http_server/responses.py
@@ -7,20 +7,32 @@ from .tokens import HTTP_VERSION, CRLF
 class Response:
 
     # https://tools.ietf.org/html/rfc2616#section-6.1.1
-    _code_phrases = {200: 'OK'}
+    _code_phrases = {200: 'OK', 500: 'Internal Server Error'}
 
     _content_types = (('text', 'html'), ('text', 'plain'))
 
     def __init__(
             self,
             status_code: int,
-            content_type: Tuple[str, str],
-            message_body: str) -> None:
+            content_type: Tuple[str, str] = None,
+            message_body: str = None) -> None:
 
         if status_code not in self._code_phrases:
             raise ValueError()
 
-        if content_type not in self._content_types:
+        if (content_type is not None
+                and content_type not in self._content_types):
+            raise ValueError()
+
+        if status_code == 200:
+            if content_type is None:
+                content_type = ('text', 'plain')
+
+            if message_body is None:
+                message_body = ''
+
+        if (status_code == 500
+                and (content_type is not None or message_body is not None)):
             raise ValueError()
 
         self._status_code = status_code
