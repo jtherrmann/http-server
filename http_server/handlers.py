2bc01e38b4ea25f118f92199bb808c058652a124
Jake Herrmann <jtherrmann@alaska.edu>
2019-03-12 16:15:58 -0800

    Pass test_handler_code_500.

--- http_server/handlers.py
+++ http_server/handlers.py
@@ -6,8 +6,13 @@ from .responses import Response
 
 def handler(f: Callable[[Request], Response]) -> Callable[[bytes], bytes]:
     def wrapper(request: bytes) -> bytes:
-        parsed_request = parse(request.decode())
-        assert parsed_request is not None  # TODO: handle with proper response
-        response_str = f(parsed_request).get_str()
+        try:
+            parsed_request = parse(request.decode())
+            assert parsed_request is not None  # TODO: handle with proper response  # noqa E501
+            response_str = f(parsed_request).get_str()
+        except:  # noqa E722
+            # TODO: log exception
+            # https://tools.ietf.org/html/rfc2616#section-10.5.1
+            response_str = Response(500).get_str()
         return response_str.encode()
     return wrapper
