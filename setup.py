61ba598b63b9b068f80723bb1ea3fd91312003d2
Jake Herrmann <jtherrmann@alaska.edu>
2019-04-01 22:46:24 -0800

    Add an http-client script.

--- setup.py
+++ setup.py
@@ -6,7 +6,10 @@ import setuptools
 setuptools.setup(
     name='http_server',
     packages=['http_server'],
-    scripts=[os.path.join('scripts', 'http-server')],
+    scripts=[
+        os.path.join('scripts', 'http-server'),
+        os.path.join('scripts', 'http-client')
+    ],
     install_requires=['attrs>=18.2.0'],
     python_requires='>=3.5.3'
 )
