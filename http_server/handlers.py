c760f9f7842bb55b53b6f6be5a9d7bc983581cd0
Jake Herrmann <jtherrmann@alaska.edu>
2019-04-01 18:55:10 -0800

    Pass test_handler_code_400.

--- http_server/handlers.py
+++ http_server/handlers.py
@@ -4,12 +4,16 @@ from .requests import Request, parse
 from .responses import Response
 
 
+# TODO: rename f
+# TODO: refactor to call get_str only at end
 def handler(f: Callable[[Request], Response]) -> Callable[[bytes], bytes]:
     def wrapper(request: bytes) -> bytes:
         try:
             parsed_request = parse(request.decode())
-            assert parsed_request is not None  # TODO: handle with proper response  # noqa E501
-            response_str = f(parsed_request).get_str()
+            if parsed_request is None:
+                response_str = Response(400).get_str()
+            else:
+                response_str = f(parsed_request).get_str()
         except:  # noqa E722
             # TODO: log exception
             # https://tools.ietf.org/html/rfc2616#section-10.5.1
