1ed7f5c3de24c919f4c870b1dcdd8e1379856253
Jake Herrmann <jtherrmann@alaska.edu>
2019-04-01 19:45:18 -0800

    Refactor handlers.handler

--- http_server/handlers.py
+++ http_server/handlers.py
@@ -4,19 +4,18 @@ from .requests import Request, parse
 from .responses import Response
 
 
-# TODO: rename f
-# TODO: refactor to call get_str only at end
-def handler(f: Callable[[Request], Response]) -> Callable[[bytes], bytes]:
+def handler(
+        handler_func: Callable[[Request], Response]) -> Callable[[bytes], bytes]:  # noqa E501
     def wrapper(request: bytes) -> bytes:
         try:
             parsed_request = parse(request.decode())
             if parsed_request is None:
-                response_str = Response(400).get_str()
+                response = Response(400)
             else:
-                response_str = f(parsed_request).get_str()
+                response = handler_func(parsed_request)
         except:  # noqa E722
             # TODO: log exception
             # https://tools.ietf.org/html/rfc2616#section-10.5.1
-            response_str = Response(500).get_str()
-        return response_str.encode()
+            response = Response(500)
+        return response.get_str().encode()
     return wrapper
