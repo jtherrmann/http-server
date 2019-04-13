65c743057e3911d7b348d44b43e6b14a46924153
Jake Herrmann <jtherrmann@alaska.edu>
2019-04-13 14:44:12 -0800

    Add jth- prefix to scripts.

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setuptools.setup(
     name='http_server',
     packages=['http_server'],
     scripts=[
-        os.path.join('scripts', 'http-server'),
-        os.path.join('scripts', 'http-client')
+        os.path.join('scripts', 'jth-http-server'),
+        os.path.join('scripts', 'jth-http-client')
     ],
     install_requires=['attrs>=18.2.0'],
     python_requires='>=3.5.3'
